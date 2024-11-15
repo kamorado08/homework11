eb25508f639387e284307ce98308a153acad4553
kamorado08 <138255477+kamorado08@users.noreply.github.com>
2024-11-15 09:49:20 -0600

    Initial commit

---
+++ src/components/MainCardSingle.vue
@@ -0,0 +1,28 @@
+<script setup>
+//importing faker to create random names, pictures, and bios 
+import { faker } from '@faker-js/faker'
+
+const firstname = faker.person.firstName()
+const lastname = faker.person.lastName()
+const fullname = faker.person.fullName({firstName: firstname, lastName: lastname})
+const job_title = faker.person.jobTitle()
+const bio = faker.lorem.lines(2)
+const username = faker.internet.userName({firstName: firstname, lastName: lastname}).toLowerCase()
+
+
+</script>
+    
+<template>
+    <!-- {{ fullname }}<br /> -->
+    <RouterLink :to="{name: 'CardDetails', params: {id: username} }">
+    <div class="border border-gray-400 rounded-xl shadow bg-gray-200 h-full">
+        <img :src="faker.image.urlLoremFlickr({width: 640, height: 480, category: 'cat'})" class="rounded-t-xl" />
+        <div class="p-4">
+            <p class="mb-2 text-2xl font-bold">{{ fullname }}</p>
+            <p class="mb-3 font-normal text-gray-700">{{ job_title }}</p>
+            <p class="mb-3 font-normal text-gray-700">{{ bio }}</p>
+        </div>
+    </div>
+    </RouterLink>
+
+</template>
